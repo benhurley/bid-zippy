0a78706647a77df09e1d3e4d144b311043423158
Ben Hurley <benfromtech@gmail.com>
2024-03-11 00:00:30 -0400

    SEO updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ export const metadata: Metadata = {
   title: "Most Watched | Browse eBay's most-watched items",
   description: "Browse eBay's most-watched items",
   applicationName: 'most-watched',
-  authors: [{name: "justbenfyi", url: 'https://justbenfyi'}],
+  authors: [{ name: "justbenfyi", url: 'https://justben.fyi' }],
   creator: "@justbenfyi",
-  keywords: ['ebay', 'watched', 'watchcount', 'search', 'browse'],
+  keywords: ['ebay', 'watch', 'saved', 'watched', 'watchcount', 'search', 'browse'],
   twitter: { card: "summary_large_image", creator: "@justbenfyi", "images": "/me.webp" },
 };
 
@@ -20,13 +20,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-        <head>
-          <meta property="og:title" content="Most Watched | Browse eBay's most-watched items" />
-          <meta property="og:description" content="Browse eBay's most-watched items" />
-          <meta property="og:image" content="/me.webp" />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://mostwatched.justben.fyi" />
-        </head>
+      <head>
+        <meta property="canonical" content="https://mostwatched.justben.fyi" />
+
+        <meta property="og:title" content="Most Watched | Browse eBay's most-watched items" />
+        <meta property="og:description" content="Browse eBay's most-watched items" />
+        <meta property="og:image" content="/me.webp" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://mostwatched.justben.fyi" />
+
+        <meta property="twitter:card" content="https://mostwatched.justben.fyi/me.webp" />
+        <meta property="twitter:title" content="Most Watched" />
+        <meta property="twitter:site" content="https://mostwatched.justben.fyi" />
+        <meta property="twitter:creator" content="@justbenfyi" />
+        <meta property="twitter:description" content="Most Watched | Browse eBay's most-watched items." />
+        <meta property="twitter:image" content="https://mostwatched.justben.fyi/me.webp" />
+
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
