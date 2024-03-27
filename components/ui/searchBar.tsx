f8a5c4c021fabb9ba5446f90a660e3ffc086906e
Ben Hurley <benjaminhurley@pm.me>
2024-03-26 23:54:07 -0400

    fix: remove window width listener

--- components/ui/searchBar.tsx
+++ components/ui/searchBar.tsx
@@ -36,7 +36,7 @@ export default function SearchBar({ handleSearch, isLoading }: TSearchBar) {
             <div className="inline-flex justify-center mb-2">
             </div>
             <div className="flex flex-col sm:flex-row items-center space-y-3 sm:space-y-0 sm:space-x-2 max-w-[400px]">
-                <Input autoFocus={window.innerWidth > 1024} type="text" className="text-base px-3 py-2 w-full sm:min-w-[350px] min-w-[315px]" placeholder="Search by Category (e.g. Watches)" onChange={handleChange} value={query} />
+                <Input type="text" className="text-base px-3 py-2 w-full sm:min-w-[350px] min-w-[315px]" placeholder="Search by Category (e.g. Watches)" onChange={handleChange} value={query} />
                 <Button disabled={isLoading || query.trim().length === 0} className="px-3 py-2 w-full sm:w-auto transition duration-300 ease-in-out lg:hover:scale-[103%]" onKeyDown={handleKeyDown} onClick={handleSubmit}>Search eBay</Button>
                 {query.length > 0 && hasSubmit && <Button variant='secondary' disabled={isLoading || query.trim().length === 0} className="px-3 py-2 w-full sm:w-auto transition duration-300 ease-in-out lg:hover:scale-[103%]" onKeyDown={handleKeyDown} onClick={handleClear}>Clear</Button>}
             </div>
